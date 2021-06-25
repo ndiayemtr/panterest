7633f8f1c54334bfa72f20c23bbae11422cfe320
ndiayemtr <ndiayemtr@gmail.com>
2021-06-25 09:44:34 +0000

    add create/edit form and basic navigation

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -6,7 +6,9 @@ use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PinsController extends AbstractController
 {
@@ -26,4 +28,61 @@ class PinsController extends AbstractController
         return $this->render('pins/show.html.twig', compact('pin'));
 
     }
+
+    /**
+     * @Route("pins/create", name="app_pin_create")
+     */
+    public function create(Request $request, EntityManagerInterface $em) : Response {
+        
+        $pin = new Pin;
+
+        $form = $this->createFormBuilder($pin)
+            ->add('title', null)
+            ->add('description', null, [
+                'attr' => ['rows' => 10, 'cols' => 50]
+            ])
+            ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($pin);
+                $em->flush();
+
+                return $this->redirectToRoute('app_pin_show', ['id' => $pin->getId()]);
+            }
+
+            return $this->render('pins/create.html.twig', ['form' => $form->createView()
+        ]);
+    }
+
+     /**
+     * @Route("pins/{id<[0-9]+>}/edit", priority=11, name="app_pin_edit")
+     */
+    public function edit(Request $request, Pin $pin, EntityManagerInterface $em) : Response {
+        
+        $form = $this->createFormBuilder($pin)
+            ->add('title', null)
+            ->add('description', null, [
+                'attr' => ['rows' => 10, 'cols' => 50]
+            ])
+            ->getForm();
+
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em->persist($pin);
+                $em->flush();
+
+                return $this->redirectToRoute('app_pin_show', ['id' => $pin->getId()]);
+            }
+            
+            $form = $form->createView();
+            return $this->render('pins/edit.html.twig', compact('form', 'pin')
+        );
+    }
+
+    
+
+    
 }
