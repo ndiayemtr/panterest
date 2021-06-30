6e4748e54ec3c8d0c193f9619f70e953f6a982de
ndiayemtr <ndiayemtr@gmail.com>
2021-06-30 15:38:50 +0000

    Télécharger image

--- src/Form/PinType.php
+++ src/Form/PinType.php
@@ -6,15 +6,25 @@ use App\Entity\Pin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class PinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Image (JPG or PNG file)',
+                #'allow_delete' => true,
+                'delete_label' => '...',
+                #'download_label' => '...',
+                'download_uri' => false,
+                #'image_uri' => true,
+                #'asset_helper' => true,
+            ])
             ->add('title', null)
             ->add('description', null, [
-            'attr' => ['rows' => 10, 'cols' => 50]
+            'attr' => ['rows' => 10, 'cols' => 30]
         ]);
     }
 
